28b97ed21f052444d8f5189a04bd401c3b400b5a
Emile Fugulin <code@efugulin.com>
2026-02-04 21:28:52 -0500

    Fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,31 @@
 import { defaultConfig } from "@caido/eslint-config";
+import { globalIgnores } from "eslint/config";
 
 export default [
   ...defaultConfig({
     compat: false,
   }),
   {
+    name: "Global",
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/triple-slash-reference": "off",
       "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-restricted-types": "off",
     },
   },
   {
-    ignores: ["**/index.js"]
+    name: "SDK Frontend",
+    ignores: [
+      "packages/sdk-frontend/src/index.js",
+      "packages/sdk-frontend/src/types/**"
+    ]
+  },
+  {
+    name: "QuickJS",
+    ignores: [
+      "packages/quickjs-types/src/llrt/**",
+      "packages/quickjs-types/src/extra/**"
+    ]
   }
 ];
